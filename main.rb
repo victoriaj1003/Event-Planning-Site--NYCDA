ef503e2145c3f3c0b4dd3987c2e4fc7e823dab37
Charles Dickstein <charleswdickstein@gmail.com>
2015-04-24 15:25:49 -0400

    placed header within div called 'myHeader'; minor css amendments

--- main.rb
+++ main.rb
@@ -10,17 +10,22 @@ get '/contacts' do
 end
 
 post '/contacts' do
-	m = Mandrill::API.new(ENV['MANDRILL_APIKEY'])
-
-	message = {
-		 :subject=> "Hello from the Mandrill API",
-		 :from_name=> "Your name",
-		 :text=>"Hi message, how are you?",
-		 :to=>[{:email=> "recipient@theirdomain.com", :name=> "Recipient1"}],
-		 :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
-		 :from_email=>"sender@yourdomain.com"
-	}
-	sending = m.messages.send message
+	require 'mandrill'  
+	m = Mandrill::API.new
+	message = {  
+	 :subject=> "Hello from the Mandrill API",  
+	 :from_name=> "Your name",  
+	 :text=>"Hi message, how are you?",  
+	 :to=>[  
+	   {  
+	     :email=> "recipient@theirdomain.com",  
+	     :name=> "Recipient1"  
+	   }  
+	 ],  
+	 :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",  
+	 :from_email=>"sender@yourdomain.com"  
+	}  
+	sending = m.messages.send message  
 	puts sending
 	redirect '/thanks'
 end 
