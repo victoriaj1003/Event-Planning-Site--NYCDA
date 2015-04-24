1591cc2ba6b318618414dd39501fb220501c9ed5
Charles Dickstein <charleswdickstein@gmail.com>
2015-04-24 13:18:35 -0400

    finished home page, incomplete mandrills

--- main.rb
+++ main.rb
@@ -1,5 +1,30 @@
 require 'sinatra' 
 
-get '/contact'  do
-	erb :contact
+get '/home'  do
+	erb :home
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	# inspects the message's param
+	m = Mandrill::API.new(ENV['MANDRILL_APIKEY'])
+
+	message = {
+		 :subject=> "Hello from the Mandrill API",
+		 :from_name=> "Your name",
+		 :text=>"Hi message, how are you?",
+		 :to=>[{:email=> "recipient@theirdomain.com", :name=> "Recipient1"}],
+		 :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
+		 :from_email=>"sender@yourdomain.com"
+	}
+	sending = m.messages.send message
+	puts sending
+	redirect '/thanks'
+end 
+
+get '/thanks' do
+  erb :thanks
 end
